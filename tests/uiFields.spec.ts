d07f505fbf9ca03948588695cc3d9a10176cf771
Chandler Keyes <keyeschandler@gmail.com>
2026-02-19 13:20:34 -0500

    getting radio buttons

--- tests/uiFields.spec.ts
+++ tests/uiFields.spec.ts
@@ -16,4 +16,21 @@ test.describe('Testing fields on Use the Grid form', async () => {
     const value = await emailInputField.inputValue();
     expect(value).toEqual(testEmail);
   });
+
+  test('Can select Radio buttons', async ({page}) => {
+    const usingTheGridForm = page.locator('nb-card', {hasText: 'Using the Grid'});
+    // Get radio button by label
+    const option1RadioButton = usingTheGridForm.getByLabel('Option 1');
+
+    // Click radio button
+    await option1RadioButton.check({force: true});
+
+    expect(await option1RadioButton.isChecked()).toBeTruthy();
+
+    // Get radio button by role
+    const option2RadioButton = usingTheGridForm.getByRole('radio', { name: 'Option 2' });
+    await option2RadioButton.check({force: true})
+
+    expect(await option2RadioButton.isChecked()).toBeTruthy();
+  })
 });
